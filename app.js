405b4d6a283e7e02c8ada0c77e360b9a59948739
Castro <dfcastro321@gmail.com>
2018-09-20 11:33:01 -0300

    Aula 08 - 17

---
+++ app.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var teste = require("./module_teste")();
+
+app.set('view engine', 'ejs');
+
+app.get("/", function (req, res) {
+    res.render("home/index");
+});
+
+app.get("/formulario", function (req, res) {
+    res.render("admin/form_add_noticia");
+});
+
+app.get("/noticias", function (req, res) {
+    res.render("noticias/noticias");
+});
+
+app.listen(3000, function () {
+    console.log("Está OK!", teste);
+});
+
---
+++ module_teste.js
@@ -0,0 +1,4 @@
+module.exports = function(){
+    var msg = "Este módulo contem apenas uma String!";
+    return msg;
+};
